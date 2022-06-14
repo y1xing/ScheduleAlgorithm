e500d99595c4d639a7bf84c158a0d904d452003f
yixing <royalcheng99@gmail.com>
2022-06-15 02:50:27 +0800

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import algorithm as A
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == "GET":
+        return "Hello"
+
+
+@app.route("/api/<unique_id>", methods=['GET', 'POST'])
+def api(unique_id):
+    if request.method == "GET":
+        schedule = A.ScheduleAlgo(unique_id=unique_id)
+        schedule.terminal_state(schedule.start)
+        schedule.solve()
+        best_schedules = A.get_best_outcomes(schedule.solution)
+        roster = best_schedules[0]['state'][0]
+        return roster.to_json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
